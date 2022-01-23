5b9e146f0ec22e6af68cf275d90dd8cef7571feb
Isabella Macht <isabella.macht@yahoo.com>
2022-01-23 16:11:24 +0100

    fixed imports in Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import React, { useState, useEffect, useLayoutEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Forecast.css";
 import ForecastDay from "./ForecastDay";
 
